25fba75496005b7c4c9ee7d9e935e2b6a61d3b4c
HectorSSG <hectorsalinasgalindo@gmail.com>
2026-02-25 21:51:07 -0500

    AKOFHJKFHek

---
+++ Assets/Scripts/SawMovement.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawMovement : MonoBehaviour
+{
+    [SerializeField] private Transform Saw;
+    [SerializeField] private List<Transform> targets = new List<Transform>();
+}
