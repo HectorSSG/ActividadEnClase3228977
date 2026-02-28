8213df91912b25e4a3efed16217ac838527e75d3
yeimipadilla085-debug <hectorsalinasgalindo@gmail.com>
2026-02-28 11:55:12 -0500

    dsfdfdf

--- Assets/Scripts/SawMovement.cs
+++ Assets/Scripts/SawMovement.cs
@@ -5,4 +5,23 @@ public class SawMovement : MonoBehaviour
 {
     [SerializeField] private Transform Saw;
     [SerializeField] private List<Transform> targets = new List<Transform>();
+    [SerializeField] private float speed;
+    [SerializeField] private int currTarget;
+    private void Start()
+    {
+        Saw.position = targets[0].position;
+        currTarget = 0;
+    }
+    private void Update()
+    {
+        if (Saw.position == targets[currTarget].position)
+        {
+            currTarget++;
+        }
+        if (currTarget >= targets.Count)
+        {
+            currTarget = 0;
+        }
+        Saw.position = Vector2.MoveTowards(Saw.position, targets[currTarget].position, Time.deltaTime * speed);
+    }
 }
